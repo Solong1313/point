01c4b71d7e72442dd2013e39facda47a2137200d
Abuddabi <web.abudabi@gmail.com>
2020-04-01 20:52:04 +0300

    update

--- handler2.php
+++ handler2.php
@@ -1,56 +1,45 @@
 <?php
 session_start();
-
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+require_once "conf.php";
 
 if (($_POST['password'] != $_POST['password_confirmation'])){
     $_SESSION['error']['password_confirmation'] = "Пароли не совпадают";
-
-        header("location: register.php");
+    header("location: register.php");
 }
 elseif(empty($_POST['name']) && empty($_POST['email']) &&  empty($_POST['password'])){
     $_SESSION['error']['name'] = "Заполните поле";
     $_SESSION['error']['email'] = "Заполните поле";
     $_SESSION['error']['password'] = "Заполните поле";
-
-        header("location: register.php");
+    header("location: register.php");
 }
 elseif (empty($_POST['name'])){
     $_SESSION['error']['name'] = "Заполните поле";
-
-             header("location: register.php");
+    header("location: register.php");
 }
 
 elseif (empty($_POST['email'])){
     $_SESSION['error']['email'] = "Заполните поле";
-
-            header("location: register.php");
+    header("location: register.php");
 }
 
 elseif (empty($_POST['password'])){
     $_SESSION['error']['password'] = "Заполните поле";
-
-            header("location: register.php");
+    header("location: register.php");
 }
 else{
     require_once 'database.php';
-
-    $Name = $_POST['name'];
+    $name = $_POST['name'];
     $mail = $_POST['email'];
-    $Password = $_POST['password'];
-    $password_2 = $_POST['password_confirmation'];
+    $password = $_POST['password'];
 
-//подготовка и выполнение запроса
+    //подготовка и выполнение запроса
     $sql ='INSERT INTO register (Name, Mail_Address, Password ) VALUES  (?,?,?) ';
     $sql = 'SELECT * from register where e-Mail_Address = $_POST[email]';
 
     $statement = $pdo->prepare($sql);
-    $result = $statement->execute([$Name, $mail, $Password]);
+    $result = $statement->execute([$name, $mail, $password]);
 
     $_SESSION['name'] ['email'] ['password']= 'Регистрация прошла успешно!';
-
-            header("location: register.php");
+    header("location: register.php");
 }
 
